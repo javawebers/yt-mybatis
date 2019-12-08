79e5c7b62fb896710cb85296bf301a2fb99e1dcb
likeabook <likeabook@163.com>
2019-12-08 15:54:44 +0800

    生成的 mapper 带 @Repository 注解

--- src/test/java/com/github/yt/mybatis/business/dao/DbEntityNotSameMapper.java
+++ src/test/java/com/github/yt/mybatis/business/dao/DbEntityNotSameMapper.java
@@ -3,12 +3,15 @@ package com.github.yt.mybatis.business.dao;
 import com.github.yt.mybatis.business.entity.DbEntityNotSame;
 import com.github.yt.mybatis.mapper.BaseMapper;
 import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
+
 
 /**
  * DbEntityNotSame Mapper
  *
  */
 @Mapper
+@Repository
 public interface DbEntityNotSameMapper extends BaseMapper<DbEntityNotSame> {
 
 
